2c072d3f93d83ab50d326b337d1f9707f92e1727
natan <72627669+natkai2040@users.noreply.github.com>
2025-12-08 12:54:33 -0500

    Add extended trail_search

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 import geopandas as gpd
@@ -133,3 +134,87 @@ def search_trails_by_name(
     results_list = simplified.to_dict(orient="records")
 
     return {"count": len(results_list), "results": results_list}
+
+def species_search(species_list, current_month, iNat_endpoint, place_ID = 2):
+    '''Helper function to run iNaturalist API queries. Default search is within place_ID=2 (MA). Returns JSON'''
+    #Get adjecent months
+    curr_month = current_month
+    if curr_month == 1:
+        last_month = 12
+    else:
+        last_month = curr_month - 1
+    if curr_month == 12:
+        next_month = 1
+    else:
+        next_month = curr_month + 1
+    #Construct iNat request
+    params = {
+        "place_id": place_ID,
+        "quality_grade": "research",
+        "per_page": 200,
+        "page": 1,
+        "month": [last_month, curr_month, next_month],        
+        "year": [2022, 2023, 2024],     # last 3 years
+        "order_by": "observed_on",
+        "taxon_id": species_list
+    }
+    r = requests.get(iNat_endpoint, params=params)
+    print(r.url)
+    return r.json()
+
+def iNatGrid_to_coords(json):
+    '''Helper. Converts iNaturalist Grid json into a list of coordinates [long, lat]'''
+    json_info = json["data"]
+    coordinate_list = []
+    for data in json_info.values():
+        coordinate = [data["longitude"], data["latitude"]]
+        coordinate_list.append(coordinate)
+    return coordinate_list
+
+def get_buffer(coordinate_list):
+    '''Helper, makes buffers'''
+    BUFFER = 5000
+    points = [Point(lon, lat) for lon, lat in coordinate_list]
+    gdf = gpd.GeoDataFrame(geometry=points, crs=PROJECTION)
+    gdf = gdf.to_crs(5070) #To metered projection
+    gdf = gdf.geometry.buffer(BUFFER) #Buffer by 200 meters
+    dissolved = gdf.union_all() #Combines all the geometries into one file
+    return gpd.GeoDataFrame(geometry=[dissolved], crs=5070).to_crs(PROJECTION)
+
+# example request: /extended_trail_search?current_month=5&include_ids=123,456&exclude_ids=789&trail_name=Blue%20Trail
+@app.get("/extended_trail_search")
+def get_trail_by_species(
+    current_month: int = Query(..., ge=1, le=12, description="Current month as integer 1-12"),
+    include_ids: List[str] = Query(None, description="List of species IDs to include"),
+    exclude_ids: List[int] = Query(None, description="List of species IDs to exclude"),
+    trail_name: str = Query('', description="Exact trail name to search for")
+):
+    #Filter trail by name
+    if (trail_name != ''):
+        trail = get_trail_by_name(trails, trail_name)
+    else:
+        trail = trails
+
+    #Construct include and exclude buffers
+    buffers = []
+    for flagged_list in [include_ids, exclude_ids]:
+        if len(flagged_list) == 0:
+            buffers.append(-2) #Indicating that no buffer was generated because no species were specified
+            continue
+        print("Generating Buffer")
+        json = species_search(flagged_list, current_month, "https://api.inaturalist.org/v1/grid/7/38/47.grid.json")
+        coords = iNatGrid_to_coords(json)
+        buffer = get_buffer(coords)
+        buffers.append(buffer)
+    
+    #Filter trails by the generated buffers
+    if type(buffers[0]) == int: #If no species in include list
+        filtered_trail = trail
+    else:
+        in_mask = trail.intersects(buffers[0].geometry.iloc[0])
+        filtered_trail = trail[in_mask].copy()
+    if type(buffers[1]) != int: #If no species in exclude list skip code below
+        out_mask = ~filtered_trail.intersects(buffers[1].geometry.iloc[0])
+        filtered_trail = filtered_trail[out_mask].copy()
+    return filtered_trail
+
